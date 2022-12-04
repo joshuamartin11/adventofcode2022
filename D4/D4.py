a2acaeccd484dd790cb0fe23ce907414a2719a49
Joshua Martin <77560324+joshuamartin11@users.noreply.github.com>
2022-12-04 15:15:46 +1000

    Completed Day 4

---
+++ D4/D4.py
@@ -0,0 +1,32 @@
+from pyparsing import Or
+
+
+with open('D4/input.txt') as f:
+    lines = f.readlines()
+
+#Part 1
+count = 0
+for line in lines:
+    assignments = line.split(',')
+    assi1 = assignments[0].split('-')
+    assi2 = assignments[1].split('-')
+
+    if (int(assi1[0]) >= int(assi2[0]) and int(assi1[1]) <= int(assi2[1])) or (int(assi2[0]) >= int(assi1[0]) and int(assi2[1]) <= int(assi1[1])):
+        print(assignments)
+        count += 1
+print(count)
+
+#Part 2
+count = 0
+for line in lines:
+    assignments = line.split(',')
+    assi1 = assignments[0].split('-')
+    assi2 = assignments[1].split('-')
+
+    assi1List = [*range(int(assi1[0]),int(assi1[1])+1,1)]
+    assi2List = [*range(int(assi2[0]),int(assi2[1])+1,1)]
+    for num in assi1List:
+        if num in assi2List:
+            count += 1
+            break
+print(count)
