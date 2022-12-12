eeb705a400f13e418e46961f04a4b971a49c1203
Joshua Martin <77560324+joshuamartin11@users.noreply.github.com>
2022-12-12 19:33:26 +1000

    Fixed Some issues and added a display

--- D12/D12p1.py
+++ D12/D12p1.py
@@ -3,7 +3,7 @@ def findNeighbours(n, board):
     # print(len(board[0]))
     neighbours = [coord for coord in neighbours if coord[0] >= 0 and coord[1] >= 0 and coord[0] < len(board) and coord[1] < len(board[0])-1]
     # print(neighbours)
-    neighbours = [coord for coord in neighbours if ord(board[n[0]][n[1]]) - ord(board[coord[0]][coord[1]]) >= -1 or (board[n[0]][n[1]] == "z" and board[coord[0]][coord[1]] == "E") or (board[n[0]][n[1]] == "S" and board[coord[0]][coord[1]] == "a")]
+    neighbours = [coord for coord in neighbours if (ord(board[n[0]][n[1]]) - ord(board[coord[0]][coord[1]]) >= -1 and ord(board[n[0]][n[1]]) - ord(board[coord[0]][coord[1]]) < 26) or (board[n[0]][n[1]] == "z" and board[coord[0]][coord[1]] == "E") or (board[n[0]][n[1]] == "S" and board[coord[0]][coord[1]] == "a") or (board[n[0]][n[1]] == "y" and board[coord[0]][coord[1]] == "E")]
 
     return(neighbours)
 with open('D12/input.txt') as f:
@@ -29,7 +29,7 @@ while len(queue) != 0:
         print(len(path) - 1)
         # for p in path:
         #     print(lines[p[0]][p[1]])
-        print(path)
+        # print(path)
         break
     for neighbour in findNeighbours(node,lines):
         if neighbour not in explored:
@@ -39,12 +39,22 @@ while len(queue) != 0:
             queue.append(newPath)
 
 
-for p in path:
+display = [["."] * len(lines[0]) for _ in range(len(lines))]
+
+display[path[-1][0]][path[-1][1]] = 'E'
+
+
+for i, p in enumerate(path[0:-1]):
     new = list(lines[p[0]])
 
-    new[p[1]] = "#"
-
-    lines[p[0]] = ''.join(new)
-
-for line in lines:
-    print(line.strip())
+    if (p[0] < path[i+1][0]):
+        display[p[0]][p[1]] = "v"
+    elif(p[0] > path[i+1][0]):
+        display[p[0]][p[1]] = "^"
+    elif(p[1] < path[i+1][1]):
+        display[p[0]][p[1]] = ">"
+    else:
+        display[p[0]][p[1]] = "<"
+display[start[0]][start[1]] = 'S'
+for line in display:
+    print(''.join(line))
